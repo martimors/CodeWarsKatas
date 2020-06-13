e6027c27ca83153519ba1cef9baf7101bfb4903f
dingobar <mmorset@gmail.com>
2020-06-13 12:57:30 +0200

    EnoughIsEnough

---
+++ EnoughIsEnough/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnoughIsEnough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Kata.DeleteNth(new int[] { 20, 37, 20, 21 }, 1)));
+        }
+        public class Kata
+        {
+            public static int[] DeleteNth(int[] arr, int x)
+            {
+                var l = new List<int>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    int v = l.Count(n => n == arr[i]);
+                    if (v < x) l.Add(arr[i]);
+                }
+                return l.ToArray();
+            }
+        }
+    }
+}
