5cf8a4b97f119bac831d2be07d86349743b07232
dingobar <mmorset@gmail.com>
2020-06-13 19:13:46 +0200

    scaffolded some tests for skyscraper

---
+++ Skyscrapers4by4.Tests/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Skyscrapers4by4.Program;
+
+using Xunit;
+
+namespace Skyscrapers4by4.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void SolveSkyscrapers1()
+        {
+            var clues = new[]{ 2, 2, 1, 3,
+                           2, 2, 3, 1,
+                           1, 2, 2, 3,
+                           3, 2, 1, 3};
+
+            var expected = new[]{ new []{1, 3, 4, 2},
+                               new []{4, 2, 1, 3},
+                               new []{3, 4, 2, 1},
+                               new []{2, 1, 3, 4 }};
+
+            var actual = Skyscrapers.SolvePuzzle(clues);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SolveSkyscrapers2()
+        {
+            var clues = new[]{ 0, 0, 1, 2,
+                                       0, 2, 0, 0,
+                                       0, 3, 0, 0,
+                                       0, 1, 0, 0};
+
+            var expected = new[]{ new []{2, 1, 4, 3},
+                               new []{3, 4, 1, 2},
+                               new []{4, 2, 3, 1},
+                               new []{1, 3, 2, 4}};
+
+            var actual = Skyscrapers.SolvePuzzle(clues);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Skyscrapers4by4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Skyscrapers4by4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public class Skyscrapers
+        {
+            public static int[][] SolvePuzzle(int[] clues)
+            {
+                // Start your coding here...
+                return null;
+            }
+        }
+
+    }
+}
