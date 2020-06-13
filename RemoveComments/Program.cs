3b7a70868a054f10e0be5a3e6c7d42f682596c66
dingobar <mmorset@gmail.com>
2020-06-13 15:32:54 +0200

    temp before trim whitespace

---
+++ RemoveComments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RemoveComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StripCommentsSolution.StripComments("asdhiasd #39458\n\n123\n$gautur", new string[] { "#", "$" }));
+        }
+        public class StripCommentsSolution
+        {
+            public static string StripComments(string text, string[] commentSymbols)
+            {
+                foreach (string symbol in commentSymbols)
+                {
+                    text = Regex.Replace(text, @"\#(.*)".Replace("#", symbol), "");
+                }
+                return text;
+            }
+        }
+    }
+}
